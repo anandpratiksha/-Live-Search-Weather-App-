16fb37f9ddfc5ca4a6a4603d1d378a26bdc13ae9
anandpratiksha <apratiksha1137@gmail.com>
2021-08-18 00:37:10 +0530

    updates

--- tempreact/src/Components/Tempapp.js
+++ tempreact/src/Components/Tempapp.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./css/style.css"
 
 function Tempapp() {
+
+    const [city, setCity] = useState(null);
     return (
         <div className="box">
             <div className="inputData">
@@ -14,8 +16,19 @@ function Tempapp() {
                 />
             </div>
             <div className="info">
-                <h2 className="location"></h2>
+                <h2 className="location">
+                    <i class="fas fa-street-view">{city}</i>
+                </h2>
+                <h1 className="temp">
+
+                </h1>
+                <h3 className="tempmin_max">
+                    Min : 5.25C
+                </h3>
             </div>
+            <div className="wave one"></div>
+            <div className="wave two"></div>
+            <div className="wave three"></div>
         </div>
     )
 }
